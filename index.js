5695a357db074d0c5d5305dcfbeaf2128c1414f6
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-31 10:57:17 -0700

    added message attribute to json sent from get to /

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
+require("dotenv").config();
 const server = express();
 
 server.use(express.json());
 
 server.get("/", (req, res) => {
-  res.status(200)/*.send("<h1>Welcome to the Greeter Program!!</h1>")*/.json({hello: "Web 23"});
+  res.status(200)/*.send("<h1>Welcome to the Greeter Program!!</h1>")*/.json({hello: "Web 23", message: process.env.MSG});
 });
 
 // The port will be set by Heroku on deployment
