f52ef0f59bfb7b92769f991442bc73414eb06fcd
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-31 10:24:37 -0700

    ran create react app, made port dynamic in index.js, added start script to package.json for deployment to heroku

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ server.get("/", (req, res) => {
   res.status(200)/*.send("<h1>Welcome to the Greeter Program!!</h1>")*/.json({hello: "Web 23"});
 });
 
-server.listen(5000, () => {
+// The port will be set by Heroku on deployment
+const port = process.env.PORT || 5000;
+console.log(`\nPort in use: ${port}\n`);
+
+server.listen(port, () => {
   console.log("\n**** Server running on port 5000 ****\n");
 });
